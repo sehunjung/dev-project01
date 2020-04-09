95829426945b00c919f6442c386026fec5136a2a
sehunjung <spgns2@gmail.com>
2020-04-09 09:10:53 +0900

    test 200409

--- hello-world/app.js
+++ hello-world/app.js
@@ -21,7 +21,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world----q4443422 test0403',
+                message: 'hello world, test date : 200409',
                 // location: ret.data.trim()
             })
         }
